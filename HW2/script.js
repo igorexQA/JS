6f57fbd989939b431f2833827a722c0d95222bdc
igorexQA <igor.qa.absoft@gmail.com>
2022-09-05 00:31:13 +0300

    Second HW

---
+++ HW2/script.js
@@ -0,0 +1,30 @@
+let myName = "Igor";
+console.log(myName);
+
+let mySurmame = "Bogdanov";
+console.log(mySurmame);
+
+let myСity = "Odesa";
+console.log(myСity);
+
+let average_of_sum = (6 + 2 + 4 + 8) / 5;
+console.log(average_of_sum);
+
+let radius_mm = 50;
+console.log(radius_mm);
+
+let lenngthOFcircle = 2 * 3.14 * radius_mm;
+console.log(lenngthOFcircle);
+
+let areaOFcircle = Math.pow(radius_mm, 2) * 3.14; 
+console.log(areaOFcircle);
+
+/*let 2ndName = 'Jack'
+
+let @PI=3.14
+
+let Sur-name = 'bogdanov' */
+
+/*This comment for HW2 point i*/
+
+//This comment for HW2 point i too
